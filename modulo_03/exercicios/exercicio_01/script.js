20dc2a4977edce7e393cac9be67b5bf0173524e9
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-04-06 17:52:11 -0300

    atividade 10 respondida

--- modulo_03/exercicios/exercicio_01/script.js
+++ modulo_03/exercicios/exercicio_01/script.js
@@ -1,4 +1,4 @@
 // Crie um botão com um id "botao" e um parágrafo com um id "paragrafo". 
-// Ao clicar no botão, o texto do parágrafo deve ser alterado para "O botão foi clicado!"
+// Ao clicar no botão, o texto do parágrafo deve ser alterado para "O botão foi clicado!".
 
 
