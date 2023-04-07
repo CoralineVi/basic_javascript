89d0dcee1a6b2e92988ce9075f05b31774cb86e6
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-04-06 23:14:10 -0300

    exercicio 2 corrigido

--- modulo_03/exercicios/exercicio_02/script.js
+++ modulo_03/exercicios/exercicio_02/script.js
@@ -1,3 +1,15 @@
 // Crie uma imagem com um id "imagem" e faça com que, ao clicar na imagem, ela seja trocada por outra imagem.
 
+// Objetos do HTML
+let obj_imagem = window.document.querySelector("p#imagem")
+//
 
+// Listeners
+obj_imagem.addEventListener("click", mudar_imagem)
+//
+
+// Funções
+function mudar_imagem(){
+    obj_imagem.src = "shitpost2.jpg"
+}
+//
