9241305ea05af3d0c79e3852c0070351b88e2478
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-04-10 08:25:39 -0300

    comeco do exercicio 3

--- modulo_03/exercicios/exercicio_03/script.js
+++ modulo_03/exercicios/exercicio_03/script.js
@@ -1,4 +1,16 @@
-// Exercício de manipulação de eventos: Crie uma lista com 5 itens e faça com que, 
+// Crie uma lista com 5 itens e faça com que, 
 // ao clicar em cada item da lista, o texto do item seja exibido em um alerta.
 
+// Objetos do HTML
 
+//
+
+// Listeners
+
+//
+
+// Função
+function botao_press(){
+    
+}
+//
