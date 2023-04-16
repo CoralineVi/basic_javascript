9e75a4e4b687dfc382275ecb27ce65667ec9c433
Coraline Vitória <coraline.vitoria.cora@gmail.com>
2023-04-15 21:41:18 -0300

    atualização

---
+++ modulo_04/exercicios/exercicio_02/script.js
@@ -0,0 +1,14 @@
+// Crie um botão com um id "botao" e um parágrafo com um id "paragrafo". 
+// Ao clicar no botão, o texto do parágrafo deve ser alterado para "O botão foi clicado!".
+
+// Objetos do HTML
+
+//
+
+// Listeners
+
+//
+
+// Funções
+
+//
